ea7c51d87d0e6466acd4b288c0b18a2bfae2852f
grodier <grodier3@gmail.com>
2023-04-03 21:56:48 -0400

    Set up a start button to autoplay music, setup tw css

---
+++ app/components/WS4000.tsx
@@ -0,0 +1,5 @@
+import musicUrl from "../assets/stardust.mp3";
+
+export default function WS4000() {
+  return <audio autoPlay loop src={musicUrl} />;
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,38 +1,35 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import React, { useState } from "react";
+import WS4000 from "~/components/WS4000";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
 
+type OverlayProps = {
+  children: React.ReactNode;
+};
+
+function Overlay({ children }: OverlayProps) {
+  return <div>{children}</div>;
+}
+
 export default function Index() {
+  const [overlayVisible, setOverlayVisible] = useState(true);
+
+  function onCloseOverlay() {
+    setOverlayVisible(false);
+  }
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ fontFamily: "monospace, system-ui" }}>
+      {overlayVisible ? (
+        <Overlay>
+          <button onClick={onCloseOverlay}>Start!</button>
+        </Overlay>
+      ) : (
+        <WS4000 />
+      )}
     </div>
   );
 }
