86ea41d005e568b5f483c24ab79f8125dbbf1ed2
grodier <grodier3@gmail.com>
2023-04-04 23:04:22 -0400

    Add scrolling marquee

--- app/components/WS4000.tsx
+++ app/components/WS4000.tsx
@@ -17,24 +17,31 @@ type WS4000Props = {
 
 export default function WS4000({ currentConditions }: WS4000Props) {
   return (
-    <div>
-      <ConditionalDisplay value={currentConditions.temperature}>
-        <div className="text-outline text-white text-lg">
-          Temp: {currentConditions.temperature}
-        </div>
-      </ConditionalDisplay>
-      <ConditionalDisplay value={currentConditions.wind_gust}>
-        <div className="text-outline text-white text-lg">
-          Wind Gusts: {currentConditions.wind_gust}
-        </div>
-      </ConditionalDisplay>
-      <ConditionalDisplay value={currentConditions.dew_point}>
-        <div className="text-outline text-white text-lg">
-          Dew Point: {currentConditions.dew_point}
-        </div>
-      </ConditionalDisplay>
-
+    <div className="min-h-screen">
       <audio autoPlay loop src={musicUrl} />
+      <header></header>
+      <main>
+        <ConditionalDisplay value={currentConditions.temperature}>
+          <div className="text-outline text-white text-lg">
+            Temp: {currentConditions.temperature}
+          </div>
+        </ConditionalDisplay>
+        <ConditionalDisplay value={currentConditions.wind_gust}>
+          <div className="text-outline text-white text-lg">
+            Wind Gusts: {currentConditions.wind_gust}
+          </div>
+        </ConditionalDisplay>
+        <ConditionalDisplay value={currentConditions.dew_point}>
+          <div className="text-outline text-white text-lg">
+            Dew Point: {currentConditions.dew_point}
+          </div>
+        </ConditionalDisplay>
+      </main>
+      <footer className="sticky top-[100vh] marquee-bg text-outline text-white text-4xl py-1 h-20">
+        <div className="marquee">
+          <p>Marquee</p>
+        </div>
+      </footer>
     </div>
   );
 }
