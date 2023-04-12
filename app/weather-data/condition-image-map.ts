eb1fcd6f286a74cea3ebacc6eb65961701c5bc26
grodier <grodier3@gmail.com>
2023-04-11 21:29:43 -0400

    Refactor assets to public

--- app/weather-data/condition-image-map.ts
+++ app/weather-data/condition-image-map.ts
@@ -1,9 +1,3 @@
-import blowingSnowUrl from "../assets/currentConditions/Blowing-Snow.gif";
-import clearUrl from "../assets/currentConditions/Clear.gif";
-import cloudyUrl from "../assets/currentConditions/Cloudy.gif";
-import freezingRainSleetUrl from "../assets/currentConditions/Freezing-Rain-Sleet.gif";
-import mostlyCloudyUrl from "../assets/currentConditions/Mostly-Cloudy.gif";
-
 export function getDescriptionImage(weatherDescription: string): string {
-  return mostlyCloudyUrl;
+  return "/assets/Cloudy.gif";
 }
